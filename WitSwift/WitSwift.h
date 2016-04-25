4ad4efa0c05d454506602c65092346dfe3eae0ab
Wenbin Zhang <wenbinzhang0802@gmail.com>
2016-04-25 08:16:44 -0700

    Initial Commit

---
+++ WitSwift/WitSwift.h
@@ -0,0 +1,19 @@
+//
+//  WitSwift.h
+//  WitSwift
+//
+//  Created by Wenbin Zhang on 4/25/16.
+//  Copyright © 2016 Wenbin Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WitSwift.
+FOUNDATION_EXPORT double WitSwiftVersionNumber;
+
+//! Project version string for WitSwift.
+FOUNDATION_EXPORT const unsigned char WitSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WitSwift/PublicHeader.h>
+
+
